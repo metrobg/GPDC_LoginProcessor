6984075d22deaabb9ffb70404b985576e55bdd5f
metrobg <egg@metrobg.com>
2020-03-22 22:56:14 -0400

    Updated module with sql insert code, database connection passed in from the MessageReceiver module. Hash Map used to parse the login message into key value pairs for processing multiple messages being received from the broker

--- src/com/metrobg/activeMQ/ConsumerMessageListener.java
+++ src/com/metrobg/activeMQ/ConsumerMessageListener.java
@@ -1,23 +1,80 @@
 package com.metrobg.activeMQ;
 
 import javax.jms.*;
+import java.sql.*;
+import java.sql.Connection;
+import java.util.*;
 
 public class ConsumerMessageListener implements MessageListener {
-    private String consumerName;
-    Connection conn;
+    private Connection conn;
+    private long messageCount;
 
-    public ConsumerMessageListener(String consumerName) {
-        this.consumerName = consumerName;
-       // this.conn = connection;
+
+    public ConsumerMessageListener(String consumerName, Connection dbConnection, long kount) {
+        this.conn = dbConnection;
+        this.messageCount = kount;
+
     }
 
     public void onMessage(Message message) {
         TextMessage textMessage = (TextMessage) message;
+        int cnt = 0;
         try {
-            System.out.println(consumerName + " received "   + textMessage.getText());
-        } catch (JMSException e) {
+            while (cnt < messageCount) {
+                // System.out.println(consumerName + " received " + textMessage.getText());
+                processMessage(textMessage.getText(), conn);
+                cnt++;
+            }
+            conn.close();
+            System.out.println("Connection closed.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                if (!conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            } finally {
+                try {
+                    if (!conn.isClosed()) {
+                        conn.close();
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void processMessage(String message, Connection dbConnection) throws SQLException {
+
+        HashMap<String, String> holder;
+        holder = new HashMap();
+
+        String[] keyVals = message.split(";");
+        for (String keyVal : keyVals) {
+            String[] parts = keyVal.split("=", 2);
+            holder.put(parts[0], parts[1]);
+        }
+        try {
+            String insertStatement = "insert into GPDC_DOMAIN_LOGIN (USER_NAME,REMOTE_HOST,LOCAL_HOST,DATE_TIME) " +
+                    "VALUES(?,?,?,to_date(?,'mm/dd/yyyy HH:mi:ss AM'))";
+            PreparedStatement ps = dbConnection.prepareStatement(insertStatement);
+
+            ps.setString(1, holder.get("User"));
+            ps.setString(2, holder.get("RemoteHost"));
+            ps.setString(3, holder.get("ComputerName"));
+            ps.setString(4, holder.get("TimeStamp"));
+            ps.execute();
+            System.out.println("Record inserted");
+            conn.commit();
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
+
+
     }
 
 }
