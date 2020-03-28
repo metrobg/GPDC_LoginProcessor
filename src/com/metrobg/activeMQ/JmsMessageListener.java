2f23ec0f8920d3447125f5d1afa71d2c5f454eb6
ggraves <eggmgm@gmail.com>
2020-03-28 13:15:33 -0400

    Updated this code to be able to test message production and message receipt from any broker.

--- src/com/metrobg/activeMQ/JmsMessageListener.java
+++ src/com/metrobg/activeMQ/JmsMessageListener.java
@@ -3,18 +3,21 @@ package com.metrobg.activeMQ;
 // https://examples.javacodegeeks.com/enterprise-java/jms/jms-messagelistener-example/
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerFactory;
+import org.apache.activemq.broker.BrokerService;
 
 import javax.jms.*;
+import java.net.URI;
 
 public class JmsMessageListener {
-    private static String user = "admin";
+    /*private static String user = "admin";
     private static String password = "Ign32ORw3C4b";
-    private static String url = "tcp://192.168.144.172:61616";
-
+    private static String url = "tcp://192.168.144.172:61616";*/
+    private static String user = "admin";
+    private static String password = "admin";
+    private static String url = "tcp://192.168.10.66:61616";
     public static void main(String[] args) throws Exception {
-       /*BrokerService broker = BrokerFactory.createBroker(new URI(
-                "broker:(tcp://localhost:61616)"));
-        broker.start();  */
+
         Connection connection = null;
         try {
             // Producer
@@ -23,28 +26,35 @@ public class JmsMessageListener {
 
             connection = connectionFactory.createConnection(user,password);
             Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
-            /*
-            Queue queue = session.createQueue("customerQueue");
+
+
+            Queue queue = session.createQueue("TestQueue");
             String payload = "Important Task";
             Message msg = session.createTextMessage(payload);
             MessageProducer producer = session.createProducer(queue);
             System.out.println("Sending text '" + payload + "'");
             producer.send(msg);
-        */
 
+
+            connection.start();
             // Consumer
-            Destination destination = session.createTopic("annual");
+            Destination destination = session.createQueue("TestQueue");
             MessageConsumer consumer = session.createConsumer(destination);
+            Message message = consumer.receive(1000);
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                System.out.println("Payload: " + textMessage.getText() + "  ");
+            }
 
             // consumer.setMessageListener(new ConsumerMessageListener("Consumer"));
-            connection.start();
-            Thread.sleep(1000);
-            session.close();
+
+
         } finally {
             if (connection != null) {
                 connection.close();
+                System.out.println("Connection Closed");
             }
-            //broker.stop();
+
         }
     }
 
