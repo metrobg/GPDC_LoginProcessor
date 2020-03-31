c39a20b3eef63691e7992cd3f7ee777864495c92
metrobg <egg@metrobg.com>
2020-03-31 00:28:21 -0400

    enhanced the program to independently be able to send or receive messages by setting the boolean flag PRODUCE.

--- src/com/metrobg/activeMQ/JmsMessageListener.java
+++ src/com/metrobg/activeMQ/JmsMessageListener.java
@@ -13,36 +13,45 @@ public class JmsMessageListener {
     private static String user = "admin";
     private static String password = "admin";
     private static String url = "tcp://192.168.10.66:61616";
+
     public static void main(String[] args) throws Exception {
+        boolean PRODUCE = false;
 
         Connection connection = null;
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+        connection = connectionFactory.createConnection(user, password);
+        Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
         try {
             // Producer
-
-            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
-
-            connection = connectionFactory.createConnection(user,password);
-            Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+            if (PRODUCE) {
 
 
-            Queue queue = session.createQueue("TestQueue");
-            String payload = "User=anatasha;RemoteHost=OUT;ComputerName=GPDC-TERM;TimeStamp=3/26/2020 1:12:38 PM;in_out=IN";
-            Message msg = session.createTextMessage(payload);
-            MessageProducer producer = session.createProducer(queue);
-            System.out.println("Sending text '" + payload + "'");
-            producer.send(msg);
+                Queue queue = session.createQueue("TestQueue");
+                String payload = null;
+                String user = "user";
+                MessageProducer producer = session.createProducer(queue);
+                // send multiple test messages
+                for (int i = 1; i <= 5; i++) {
+                    payload = "User=anatasha-" + i + ";RemoteHost=OUT;ComputerName=GPDC-TERM;TimeStamp=3/26/2020 1:12:38 PM;in_out=IN";
+                    Message msg = session.createTextMessage(payload);
+                    System.out.println("Sending text '" + payload + "'");
+                    producer.send(msg);
+                }
+            }
+            if (!PRODUCE) {
+                connection.start();
+                // Consumer
+                Destination destination = session.createQueue("TestQueue");
+                MessageConsumer consumer = session.createConsumer(destination);
+                Message message = consumer.receive(1000);
+                if (message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    System.out.println("Payload: " + textMessage.getText() + "  ");
+                    //message.acknowledge();
 
-
-            //connection.start();
-            // Consumer
-            /*Destination destination = session.createQueue("TestQueue");
-            MessageConsumer consumer = session.createConsumer(destination);
-            Message message = consumer.receive(1000);
-            if (message instanceof TextMessage) {
-                TextMessage textMessage = (TextMessage) message;
-                System.out.println("Payload: " + textMessage.getText() + "  ");
-            }*/
-
+                }
+            }
             // consumer.setMessageListener(new ConsumerMessageListener("Consumer"));
 
 
